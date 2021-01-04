d43b7ba7425b348245915dfdc19a4d81257714fa
Shubhashish Kumar <shubhashish005@gmail.com>
2021-01-04 17:15:16 +0530

    Refactored the code

--- src/main/java/com/shubh/designPattern/factoryDesignPattern/factory/Computer.java
+++ src/main/java/com/shubh/designPattern/factoryDesignPattern/factory/Computer.java
@@ -3,13 +3,8 @@ package com.shubh.designPattern.factoryDesignPattern.factory;
 public interface Computer {
 
     String ram();
-
     String hdd();
-
     String cpu();
-
     Boolean isgraphicsEnabled();
-
     Boolean isBluetoothEnabled();
-
 }
